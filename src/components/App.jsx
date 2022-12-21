819a73f559ff72cb42db39be2cf3c9d1735e1a2b
EkaterinaKononenko <kaktysik4@ukr.net>
2022-12-21 12:39:52 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,77 +1,79 @@
-import React, { Component } from 'react';
+import {useState} from 'react';
 import { Filter } from './Filter';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
 import { Container, PhoneTitle, ContactTitle, IconWrapper } from './App.styled';
 import { AiOutlineApple } from 'react-icons/ai';
 import { AiOutlineUsergroupAdd } from 'react-icons/ai';
+import { useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+    const useLocalStorage = (contacts, defaultValue) => {
+      const [state, setState] = useState(() => {
+        return (
+          JSON.parse(window.localStorage.getItem('contacts')) ?? defaultValue
+        );
+      });
+      useEffect(() => {
+        localStorage.setItem('contacts', JSON.stringify(state));
+      }, [contacts, state]);
+
+      return [state, setState];
+    };
+
+export default function App() {
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useLocalStorage('contacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  
+
+
+  const onFilter = (e) => {
+    setFilter(e.target.value);
   };
 
-  onFilter = e => {
-    this.setState({ filter: e.target.value });
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  addContact = data => {
-    this.setState(({ contacts }) =>
-      contacts.find(contact => contact.name === data.name)
-        ? alert(`${data.name} is already in contacts`)
-        : { contacts: [data, ...contacts] }
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
     );
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts !== null) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  const addContact = newContact => {
+    setContacts(prevContacts => {
+      if (prevContacts.find(contact => contact.name === newContact.name)) {
+        alert(`${newContact.name} is already in contacts`);
+        return prevContacts;
+      }
+      return [newContact, ...prevContacts];
+    });
+  };
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
 
-  render() {
-    const { contacts, filter } = this.state;
-    const contactAfterFilter = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
 
-    return (
-      <Container>
-        <IconWrapper>
-          <PhoneTitle>Phonebook</PhoneTitle>
-          <AiOutlineApple color="#D71868" fontSize="3em" />
-        </IconWrapper>
-        <ContactForm onSubmit={this.addContact} />
-        <IconWrapper>
-          <ContactTitle>Contacts</ContactTitle>
-          <AiOutlineUsergroupAdd color="#D71868" fontSize="3em" />
-        </IconWrapper>
-        <Filter value={filter} onFilter={this.onFilter} />
-        <ContactList
-          contacts={contactAfterFilter}
-          deleteContact={this.deleteContact}
-        />
-      </Container>
-    );
-  }
+const contactAfterFilter = contacts.filter(contact =>
+  contact.name.toLowerCase().includes(filter.toLowerCase())
+);
+  
+  return (
+    <Container>
+      <IconWrapper>
+        <PhoneTitle>Phonebook</PhoneTitle>
+        <AiOutlineApple color="#D71868" fontSize="3em" />
+      </IconWrapper>
+      <ContactForm onSubmit={addContact} />
+      <IconWrapper>
+        <ContactTitle>Contacts</ContactTitle>
+        <AiOutlineUsergroupAdd color="#D71868" fontSize="3em" />
+      </IconWrapper>
+      <Filter value={filter} onFilter={onFilter} />
+      <ContactList
+        contacts={contactAfterFilter}
+        deleteContact={deleteContact}
+      />
+    </Container>
+  );
 }
+  
